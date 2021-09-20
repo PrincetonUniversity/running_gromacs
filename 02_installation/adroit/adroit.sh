449bede0c73c4d80309af5f2ae2d9b0ca97b9cab
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-09-20 16:57:40 -0400

    Update adroit.sh

--- 02_installation/adroit/adroit.sh
+++ 02_installation/adroit/adroit.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
-version=2019.6
-wget ftp://ftp.gromacs.org/pub/gromacs/gromacs-${version}.tar.gz
-tar -zxvf gromacs-${version}.tar.gz
-cd gromacs-${version}
-mkdir build_stage1
-cd build_stage1
 
 #############################################################
-# build gmx (for single-node jobs)
+# set the version
 #############################################################
+version=2021.3
+
+#############################################################
+# you probably don't need to change anything below this line
+#############################################################
+#wget ftp://ftp.gromacs.org/pub/gromacs/gromacs-${version}.tar.gz
+tar zxvf gromacs-${version}.tar.gz
+cd gromacs-${version}
+mkdir build
+cd build
 
 module purge
-module load intel/19.0/64/19.0.1.144
-module load cudatoolkit/10.2
-module load rh/devtoolset/7
+module load cudatoolkit/11.4
 
-OPTFLAGS="-Ofast -xCORE-AVX512 -mtune=skylake-avx512 -DNDEBUG"
+OPTFLAGS="-O3 -DNDEBUG"
 
 cmake3 .. -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_C_COMPILER=icc -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
--DCMAKE_CXX_COMPILER=icpc -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
+-DCMAKE_C_COMPILER=gcc -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
+-DCMAKE_CXX_COMPILER=g++ -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
 -DGMX_BUILD_MDRUN_ONLY=OFF -DGMX_MPI=OFF -DGMX_OPENMP=ON \
 -DGMX_SIMD=AVX_512 -DGMX_DOUBLE=OFF \
--DGMX_FFT_LIBRARY=mkl \
--DGMX_GPU=ON -DGMX_CUDA_TARGET_SM=70 \
+-DGMX_BUILD_OWN_FFTW=ON \
+-DGMX_GPU=CUDA -DGMX_CUDA_TARGET_SM=80 \
 -DCMAKE_INSTALL_PREFIX=$HOME/.local \
 -DGMX_COOL_QUOTES=OFF -DREGRESSIONTEST_DOWNLOAD=ON
 
-make -j 10
+make -j 8
 make check
 make install
