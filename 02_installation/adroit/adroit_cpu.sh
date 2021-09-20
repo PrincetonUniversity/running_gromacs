7ec776c0caed3302e7ca78801543592d24522873
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-09-20 15:17:25 -0400

    Update adroit_cpu.sh

--- 02_installation/adroit/adroit_cpu.sh
+++ 02_installation/adroit/adroit_cpu.sh
@@ -2,38 +2,34 @@
 #############################################################
 # set the version
 #############################################################
-version=2020.5
+version=2021.3
 
 #############################################################
 # you probably don't need to change anything below this line
 #############################################################
-
 wget ftp://ftp.gromacs.org/pub/gromacs/gromacs-${version}.tar.gz
-tar zxvf gromacs-${version}.tar.gz
+tar -zxvf gromacs-${version}.tar.gz
 cd gromacs-${version}
-mkdir build_stage1
-cd build_stage1
-
-#############################################################
-# build gmx (stage 1)
-#############################################################
+mkdir build && cd build
 
 module purge
-module load intel/19.0/64/19.0.1.144 rh/devtoolset/7
+module load openmpi/gcc/4.1.0
 
-OPTFLAGS="-Ofast -xCORE-AVX512 -mtune=skylake-avx512 -DNDEBUG"
+OPTFLAGS="-O3 -DNDEBUG"
 
 cmake3 .. -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_C_COMPILER=icc -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
--DCMAKE_CXX_COMPILER=icpc -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
--DGMX_BUILD_MDRUN_ONLY=OFF -DGMX_MPI=OFF -DGMX_OPENMP=ON \
+-DCMAKE_C_COMPILER=mpicc \
+-DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
+-DCMAKE_CXX_COMPILER=mpic++ \
+-DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
+-DGMX_BUILD_OWN_FFTW=ON \
+-DGMX_MPI=ON -DGMX_OPENMP=ON \
 -DGMX_SIMD=AVX_512 -DGMX_DOUBLE=OFF \
--DGMX_FFT_LIBRARY=mkl \
 -DGMX_GPU=OFF \
 -DCMAKE_INSTALL_PREFIX=$HOME/.local \
 -DGMX_DEFAULT_SUFFIX=OFF -DGMX_BINARY_SUFFIX=_cpu -DGMX_LIBS_SUFFIX=_cpu \
 -DGMX_COOL_QUOTES=OFF -DREGRESSIONTEST_DOWNLOAD=ON
 
-make -j 10
+make -j 8
 make check
 make install
