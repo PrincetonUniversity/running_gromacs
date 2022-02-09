a212acabb6f5cfef490994ef602ae643c78e140f
jdh4 <52128661+jdh4@users.noreply.github.com>
2022-02-09 10:02:38 -0500

    Update della.sh

--- 02_installation/ins/della/della.sh
+++ 02_installation/ins/della/della.sh
@@ -2,7 +2,7 @@
 #############################################################
 # set the version
 #############################################################
-version=2019.6
+version=2021.5
 
 #############################################################
 # you probably don't need to change anything below this line
@@ -19,13 +19,13 @@ cd build_stage1
 #############################################################
 
 module purge
-module load intel/19.0/64/19.0.5.281 rh/devtoolset/6
+module load intel/2021.1.2
 
-OPTFLAGS="-Ofast -xCORE-AVX2 -axCORE-AVX512 -DNDEBUG"
+OPTFLAGS="-Ofast -DNDEBUG"
 
 cmake3 .. -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_C_COMPILER=icc -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
--DCMAKE_CXX_COMPILER=icpc -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
+-DCMAKE_C_COMPILER=icx -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
+-DCMAKE_CXX_COMPILER=icpx -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
 -DGMX_BUILD_MDRUN_ONLY=OFF -DGMX_MPI=OFF -DGMX_OPENMP=ON \
 -DGMX_DOUBLE=OFF \
 -DGMX_FFT_LIBRARY=mkl \
@@ -45,11 +45,11 @@ cd ..
 mkdir build_stage2
 cd build_stage2
 
-module load intel-mpi/intel/2018.3/64
+module load intel-mpi/intel/2021.1.1
 
 cmake3 .. -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_C_COMPILER=icc -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
--DCMAKE_CXX_COMPILER=icpc -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
+-DCMAKE_C_COMPILER=icx -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
+-DCMAKE_CXX_COMPILER=icpx -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
 -DGMX_BUILD_MDRUN_ONLY=ON -DGMX_MPI=ON -DGMX_OPENMP=ON \
 -DGMX_DOUBLE=OFF \
 -DGMX_FFT_LIBRARY=mkl \
@@ -58,6 +58,4 @@ cmake3 .. -DCMAKE_BUILD_TYPE=Release \
 -DGMX_COOL_QUOTES=OFF -DREGRESSIONTEST_DOWNLOAD=ON
 
 make -j 10
-source ../build_stage1/scripts/GMXRC
-tests/regressiontests-${version}/gmxtest.pl all
 make install
