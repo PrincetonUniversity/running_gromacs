980f8b32259bb779227fd7493bbdbdc51a6dcdfe
jdh4 <52128661+jdh4@users.noreply.github.com>
2023-05-10 10:57:23 -0400

    Update della.sh

--- 02_installation/ins/della/della.sh
+++ 02_installation/ins/della/della.sh
@@ -2,7 +2,7 @@
 #############################################################
 # set the version
 #############################################################
-version=2021.5
+version=2023.1
 
 #############################################################
 # you probably don't need to change anything below this line
@@ -11,51 +11,25 @@ version=2021.5
 wget ftp://ftp.gromacs.org/pub/gromacs/gromacs-${version}.tar.gz
 tar zxvf gromacs-${version}.tar.gz
 cd gromacs-${version}
-mkdir build_stage1
-cd build_stage1
-
-#############################################################
-# build gmx (stage 1)
-#############################################################
+mkdir build && cd build
 
 module purge
-module load intel/2021.1.2
+module load gcc-toolset/12
+module load intel-mkl/2022.2.0
+module load openmpi/gcc/4.1.2
 
 OPTFLAGS="-O3 -DNDEBUG"
 
 cmake3 .. -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_C_COMPILER=icx -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
--DCMAKE_CXX_COMPILER=icpx -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
--DGMX_BUILD_MDRUN_ONLY=OFF -DGMX_MPI=OFF -DGMX_OPENMP=ON \
+-DCMAKE_C_COMPILER=mpicc -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
+-DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
+-DGMX_MPI=ON -DGMX_OPENMP=ON \
 -DGMX_DOUBLE=OFF \
 -DGMX_FFT_LIBRARY=mkl \
 -DGMX_GPU=OFF \
 -DCMAKE_INSTALL_PREFIX=$HOME/.local \
 -DGMX_COOL_QUOTES=OFF -DREGRESSIONTEST_DOWNLOAD=ON
 
-make -j 10
+make -j 8
 make check
-make install
-
-#############################################################
-# build mdrun_mpi (stage 2)
-#############################################################
-
-cd ..
-mkdir build_stage2
-cd build_stage2
-
-module load intel-mpi/intel/2021.1.1
-
-cmake3 .. -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_C_COMPILER=icx -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
--DCMAKE_CXX_COMPILER=icpx -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
--DGMX_BUILD_MDRUN_ONLY=ON -DGMX_MPI=ON -DGMX_OPENMP=ON \
--DGMX_DOUBLE=OFF \
--DGMX_FFT_LIBRARY=mkl \
--DGMX_GPU=OFF \
--DCMAKE_INSTALL_PREFIX=$HOME/.local \
--DGMX_COOL_QUOTES=OFF -DREGRESSIONTEST_DOWNLOAD=OFF
-
-make -j 10
 make install
