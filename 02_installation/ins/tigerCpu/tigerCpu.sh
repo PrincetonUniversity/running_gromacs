5acfb88426abe2e10b0b9da2f7e9e5c3c5d66b1a
Jonathan Halverson <52128661+jdh4@users.noreply.github.com>
2024-10-24 13:48:40 -0400

    Update tigerCpu.sh

--- 02_installation/ins/tigerCpu/tigerCpu.sh
+++ 02_installation/ins/tigerCpu/tigerCpu.sh
@@ -2,7 +2,7 @@
 #############################################################
 # set the version
 #############################################################
-version=2019.6
+version=2024.3
 
 #############################################################
 # you probably don't need to change anything below this line
@@ -19,22 +19,28 @@ cd build_stage1
 #############################################################
 
 module purge
-module load intel/19.0/64/19.0.5.281 rh/devtoolset/7
+module load intel-oneapi/2024.2
+module load intel-mkl/2024.2
 
-OPTFLAGS="-Ofast -xCORE-AVX512 -mtune=skylake-avx512 -DNDEBUG"
+OPTFLAGS="-Ofast -xsapphirerapids -DNDEBUG"
 
 cmake3 .. -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_C_COMPILER=icc -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
--DCMAKE_CXX_COMPILER=icpc -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
--DGMX_BUILD_MDRUN_ONLY=OFF -DGMX_MPI=OFF -DGMX_OPENMP=ON \
--DGMX_SIMD=AVX_512 -DGMX_DOUBLE=OFF \
+-DCMAKE_C_COMPILER=icx \
+-DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
+-DCMAKE_CXX_COMPILER=icpx \
+-DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
+-DGMX_BUILD_MDRUN_ONLY=OFF \
+-DGMX_MPI=OFF \
+-DGMX_OPENMP=ON \
+-DGMX_SIMD=AVX_512 \
+-DGMX_DOUBLE=OFF \
 -DGMX_FFT_LIBRARY=mkl \
 -DGMX_GPU=OFF \
 -DCMAKE_INSTALL_PREFIX=$HOME/.local \
 -DGMX_DEFAULT_SUFFIX=OFF -DGMX_BINARY_SUFFIX=_cpu -DGMX_LIBS_SUFFIX=_cpu \
 -DGMX_COOL_QUOTES=OFF -DREGRESSIONTEST_DOWNLOAD=ON
 
-make -j 10
+make -j 32
 make check
 make install
 
@@ -46,20 +52,25 @@ cd ..
 mkdir build_stage2
 cd build_stage2
 
-module load intel-mpi/intel/2019.5/64
+module load intel-mpi/oneapi/2021.13
 
 cmake3 .. -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_C_COMPILER=icc -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
--DCMAKE_CXX_COMPILER=icpc -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
--DGMX_BUILD_MDRUN_ONLY=ON -DGMX_MPI=ON -DGMX_OPENMP=ON \
--DGMX_SIMD=AVX_512 -DGMX_DOUBLE=OFF \
+-DCMAKE_C_COMPILER=icx \
+-DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
+-DCMAKE_CXX_COMPILER=icpx \
+-DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
+-DGMX_BUILD_MDRUN_ONLY=ON \
+-DGMX_MPI=ON \
+-DGMX_OPENMP=ON \
+-DGMX_SIMD=AVX_512 \
+-DGMX_DOUBLE=OFF \
 -DGMX_FFT_LIBRARY=mkl \
 -DGMX_GPU=OFF \
 -DCMAKE_INSTALL_PREFIX=$HOME/.local \
 -DGMX_DEFAULT_SUFFIX=OFF -DGMX_BINARY_SUFFIX=_cpu_mpi -DGMX_LIBS_SUFFIX=_cpu_mpi \
 -DGMX_COOL_QUOTES=OFF -DREGRESSIONTEST_DOWNLOAD=ON
 
-make -j 10
+make -j 16
 source ../build_stage1/scripts/GMXRC
 tests/regressiontests-${version}/gmxtest.pl all
 make install
