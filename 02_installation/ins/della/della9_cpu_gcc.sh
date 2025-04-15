ae0010e0d02e40c1d70c008fb3542e55cdb477fa
Jonathan Halverson <52128661+jdh4@users.noreply.github.com>
2025-04-15 11:21:21 -0400

    Create della9_cpu_gcc.sh

---
+++ 02_installation/ins/della/della9_cpu_gcc.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#############################################################
+# set the version
+#############################################################
+version=2025.1
+
+#############################################################
+# you probably don't need to make changes below
+#############################################################
+wget ftp://ftp.gromacs.org/pub/gromacs/gromacs-${version}.tar.gz
+tar zxvf gromacs-${version}.tar.gz
+cd gromacs-${version}
+mkdir build && cd build
+
+module purge
+module load cmake/3.30.8    # DO NOT INCLUDE IN SLURM SCRIPT
+module load gcc-toolset/14  # DO NOT INCLUDE IN SLURM SCRIPT
+module load openmpi/gcc/4.1.6
+
+OPTFLAGS="-O3 -DNDEBUG"
+
+which cmake
+which nvcc
+which gcc
+which mpicc
+
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER=mpicc \
+    -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
+    -DCMAKE_CXX_COMPILER=mpic++ \
+    -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
+    -DGMX_BUILD_OWN_FFTW=ON \
+    -DGMX_MPI=ON \
+    -DGMX_GPU=OFF \
+    -DGMX_OPENMP=ON \
+    -DGMX_OPENMP_MAX_THREADS=192 \
+    -DGMX_DOUBLE=OFF \
+    -DCMAKE_INSTALL_PREFIX=$HOME/.local \
+    -DREGRESSIONTEST_DOWNLOAD=ON \
+    -DGMX_DEFAULT_SUFFIX=OFF -DGMX_BINARY_SUFFIX=_d9_cpu -DGMX_LIBS_SUFFIX=_d9_cpu \
+    -DGMX_COOL_QUOTES=OFF
+
+make -j 8
+make check
+make install
